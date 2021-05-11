977653ff9059a44158b7f5b1e142d0436d192640
WesleyPin <wesley.pin.casadei@gmail.com>
2021-05-11 14:59:28 +0200

    [APP] Implentation du code pour la configuration de firebase.

--- social-network/src/app/app.component.ts
+++ social-network/src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/database";
+import "firebase/analytics";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'social-network';
+  
+  constructor() {
+    var firebaseConfig = {
+      apiKey: "AIzaSyDvznYhV3fdzW1-yiyvJVsmYSs-JdI1pdQ",
+      authDomain: "social-network-d6e79.firebaseapp.com",
+      projectId: "social-network-d6e79",
+      storageBucket: "social-network-d6e79.appspot.com",
+      messagingSenderId: "344033441253",
+      appId: "1:344033441253:web:da696bb13aaa6a04d9eb0c",
+      measurementId: "G-WKLDXGF8QC"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
