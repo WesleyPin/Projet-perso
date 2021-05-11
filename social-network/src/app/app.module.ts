17a183671feee1ca4712c8272f8bb03a68356329
WesleyPin <wesley.pin.casadei@gmail.com>
2021-05-11 15:05:20 +0200

    [APP] Création des component de départ.

--- social-network/src/app/app.module.ts
+++ social-network/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ListPostComponent } from './list-post/list-post.component';
+import { NewPostComponent } from './new-post/new-post.component';
+import { AuthComponent } from './auth/auth.component';
+import { FilterPostComponent } from './filter-post/filter-post.component';
+import { SearchUserComponent } from './search-user/search-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ListPostComponent,
+    NewPostComponent,
+    AuthComponent,
+    FilterPostComponent,
+    SearchUserComponent
   ],
   imports: [
     BrowserModule,
---
+++ social-network/src/app/filter-post/filter-post.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-post',
+  templateUrl: './filter-post.component.html',
+  styleUrls: ['./filter-post.component.scss']
+})
+export class FilterPostComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
