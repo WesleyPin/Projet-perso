5cbf9c4e79eaaa49221a01c9bf5128f25eaae00a
WesleyPin <wesley.pin.casadei@gmail.com>
2021-05-26 16:03:55 +0200

    [ADD][POST] Visuel de l'ajout d'un post

--- social-network/src/app/list-post/list-post.component.ts
+++ social-network/src/app/list-post/list-post.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import { AuthService } from '../services/auth.service';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-list-post',
@@ -7,9 +13,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListPostComponent implements OnInit {
 
-  constructor() { }
+  isAddingPost: boolean;
+  currentUser: User;
+
+  constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit(): void {
+    firebase.auth().onAuthStateChanged(
+      (user) => {
+        if (user) {
+          this.userService.getUserByUid(user.uid).then(
+            (u: User) => {
+              this.currentUser = u;
+              this.currentUser.uid = user.uid;
+            }, (error) => {
+              console.log('erreur de récupération.');
+            }
+          )
+        }
+      }
+    );
+  }
+
+  changeStatusAdd() {
+    this.isAddingPost = !this.isAddingPost;
   }
 
 }
